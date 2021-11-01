589b2711cfc25be3428a6105540794404701371b
Paweł Brzozowski <mac@MacBook-Air-Pawe.local>
2021-11-01 11:25:01 +0100

    initial commit

---
+++ WorldHolidays/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WorldHolidays
+//
+//  Created by Paweł Brzozowski on 01/11/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
